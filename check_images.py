6b96bfd6f5f360f0b4a0969295333d1328e3e99b
mkanderson71 <mkanderson71@gmail.com>
2022-06-22 15:44:26 -0400

    starting edits

--- check_images.py
+++ check_images.py
@@ -1,27 +1,25 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/check_images.py
-#
-# TODO 0: Add your information below for Programmer & Date Created.                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
-# PURPOSE: Classifies pet images using a pretrained CNN model, compares these
-#          classifications to the true identity of the pets in the images, and
-#          summarizes how well the CNN performed on the image classification task. 
-#          Note that the true identity of the pet (or object) in the image is 
-#          indicated by the filename of the image. Therefore, your program must
-#          first extract the pet image label from the filename before
-#          classifying the images using the pretrained CNN model. With this 
-#          program we will be comparing the performance of 3 different CNN model
-#          architectures to determine which provides the 'best' classification.
-#
-# Use argparse Expected Call with <> indicating expected user input:
-#      python check_images.py --dir <directory with images> --arch <model>
-#             --dogfile <file that contains dognames>
-#   Example call:
-#    python check_images_solution.py --dir pet_images/ --arch vgg --dogfile dognames.txt
-##
+"""
+UDACITY AIPND PROJECT
+
+PROGRAMMER/STUDENT: Mike Anderson
+DATE CREATED: 2022-06-22
+REVISED DATE:
+PURPOSE: Classifies pet images using a pretrained CNN model, compares these
+         classifications to the true identity of the pets in the images, and
+         summarizes how well the CNN performed on the image classification task. 
+         Note that the true identity of the pet (or object) in the image is 
+         indicated by the filename of the image. Therefore, your program must
+         first extract the pet image label from the filename before
+         classifying the images using the pretrained CNN model. With this 
+         program we will be comparing the performance of 3 different CNN model
+         architectures to determine which provides the 'best' classification.
+
+Use argparse Expected Call with <> indicating expected user input:
+    python check_images.py --dir <directory with images> --arch <model>
+            --dogfile <file that contains dognames>
+  Example call:
+    python check_images_solution.py --dir pet_images/ --arch vgg --dogfile dognames.txt
+"""
 
 # Imports python modules
 from time import time, sleep
@@ -39,14 +37,10 @@ from print_results import print_results
 
 # Main program function defined below
 def main():
-    # TODO 0: Measures total program runtime by collecting start time
+    # Measures total program runtime by collecting start time
     start_time = time()
     
-    # TODO 1: Define get_input_args function within the file get_input_args.py
-    # This function retrieves 3 Command Line Arugments from user as input from
-    # the user running the program from a terminal window. This function returns
-    # the collection of these command line arguments from the function call as
-    # the variable in_arg
+    # command line parser
     in_arg = get_input_args()
 
     # Function that checks command line arguments using in_arg  
@@ -60,7 +54,7 @@ def main():
     #             get_pet_labels(in_arg.dir)
     # This function creates the results dictionary that contains the results, 
     # this dictionary is returned from the function call as the variable results
-    results = get_pet_labels(None)
+    results = get_pet_labels(in_arg.dir)
 
     # Function that checks Pet Images in the results Dictionary using results    
     check_creating_pet_image_labels(results)
--- get_pet_labels.py
+++ get_pet_labels.py
@@ -1,21 +1,20 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/get_pet_labels.py
-#                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                  
-# REVISED DATE: 
-# PURPOSE: Create the function get_pet_labels that creates the pet labels from 
-#          the image's filename. This function inputs: 
-#           - The Image Folder as image_dir within get_pet_labels function and 
-#             as in_arg.dir for the function call within the main function. 
-#          This function creates and returns the results dictionary as results_dic
-#          within get_pet_labels function and as results within main. 
-#          The results_dic dictionary has a 'key' that's the image filename and
-#          a 'value' that's a list. This list will contain the following item
-#          at index 0 : pet image label (string).
-#
-##
+"""
+get_pet_labels.py
+
+PROGRAMMER: Mike Anderson
+DATE CREATED: 2022-06-22
+REVISED DATE: 2022-06-22
+PURPOSE: Create the function get_pet_labels that creates the pet labels from 
+         the image's filename. This function inputs: 
+          - The Image Folder as image_dir within get_pet_labels function and 
+            as in_arg.dir for the function call within the main function. 
+         This function creates and returns the results dictionary as results_dic
+         within get_pet_labels function and as results within main. 
+         The results_dic dictionary has a 'key' that's the image filename and
+         a 'value' that's a list. This list will contain the following item
+         at index 0 : pet image label (string).
+"""
+
 # Imports python modules
 from os import listdir
 
